3989805a2cf6dbf13aec0c44bc6e527b63a936b4
nareaero <narendraheart12@gmail.com>
2018-06-04 00:16:32 +0530

    name aded

--- write_in_air.py
+++ write_in_air.py
@@ -19,7 +19,9 @@ while True:
 
     # Convert from BGR to HSV color format
     hsv = cv2.cvtColor(blur_frame, cv2.COLOR_BGR2HSV)
-
+    cv2.putText(frame, "narendra_image", (0+2, 0+20), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (255, 0, 100), 2)
     # Define lower and upper range of hsv color to detect. Blue here
     lower_blue = np.array([100, 90, 50])
     upper_blue = np.array([140, 255, 255])
@@ -44,7 +46,7 @@ while True:
         cx = int(moments['m10'] / moments['m00'])
         cy = int(moments['m01'] / moments['m00'])
         centre_of_contour = (cx,cy)
-        cv2.circle(frame, centre_of_contour, 9, (0, 0, 255), -1)
+        cv2.circle(frame, centre_of_contour, 4, (0, 0, 255), -1)
 
         # Bound the contour with circle
         ellipse = cv2.fitEllipse(biggest_contour)
@@ -53,6 +55,9 @@ while True:
         # Save the center of contour so we draw line tracking it
         center_points.appendleft(centre_of_contour)
         print(centre_of_contour)
+        cv2.putText(frame, "air drawer is active", (frame.shape[1]-170, frame.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (0, 0, 255), 2)
+	
         cv2.putText(frame,("{:.2f} {:.2f}".format(cx,cy)), (cx - 20, cy - 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
     # Draw line from center points of contour
     for i in range(1, len(center_points)):
@@ -63,7 +68,7 @@ while True:
                 (center_points[i - 1][1] - center_points[i][1]) ** 2)) <= 40:
             cv2.line(frame, center_points[i - 1], center_points[i], (0, 0, 255), 4)
 
-    cv2.imshow('original', frame)
+    cv2.imshow('write_text', frame)
     # cv2.imshow('mask', mask)
 
     k = cv2.waitKey(5) & 0xFF
